935d38acd16799b33866f3210122383d2de47cba
nicola00 <nicolajobs00@gmail.com>
2023-04-04 16:56:10 +0200

    FEAT second html page contacts

---
+++ laravel/resources/views/contacts.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="card">
+                    <ul>
+                        <li>contacts</li>
+                        <li>contacts</li>
+                        <li>contacts</li>
+                        <li>contacts</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+
+<style>
+    .container {
+        margin: 1000px auto;
+
+    }
+</style>
+
+</html>
